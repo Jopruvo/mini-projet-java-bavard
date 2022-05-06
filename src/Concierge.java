a7908ba680a29dea96cbfd44e13fc7e93158c9db
jopruvo <jordan.pruvost11@gmail.com>
2022-05-06 16:05:43 +0200

    Ajout classe Concierge

--- src/Concierge.java
+++ src/Concierge.java
@@ -3,9 +3,34 @@ import java.util.ArrayList;
 
 public class Concierge {
     
-    List<Bavard> listBavards = new ArrayList<>();
+    List<Bavard> listBavardsConnected = new ArrayList<>();
+    List<Bavard> listBavardsDisconnected = new ArrayList<>();
 
     public Concierge(){
+    }
+
+    public void createBavard(Bavard b){
+        listBavardsConnected.add(b);
+    }
+
+    public void interet(Bavard b, boolean bool){
+        if(bool == true & listBavardsDisconnected.contains(b)){
+            for(int i = 0; i < listBavardsDisconnected.size(); i++){
+                if(listBavardsDisconnected.get(i) == b){
+                    listBavardsDisconnected.remove(i);
+                    listBavardsConnected.add(b);
+                }
+            }
+        }
+
+        if(bool == false & listBavardsConnected.contains(b)){
+            for(int i = 0; i < listBavardsConnected.size(); i++){
+                if(listBavardsConnected.get(i) == b){
+                    listBavardsConnected.remove(i);
+                    listBavardsDisconnected.add(b);
+                }
+            }
+        }
     }
 
     
